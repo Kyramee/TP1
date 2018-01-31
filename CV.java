f7be2186e85fba13aef9f845fbbf7e37755863a9
Kyramee <adrienthereader@gmail.com>
2018-01-31 16:27:57 -0500

    Creation class CV plus constructeur

---
+++ CV.java
@@ -0,0 +1,17 @@
+public class CV {
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int experiance;
+	private String[] competences;
+	private String attentes;
+	
+	public CV(String nom, String prenom, String formation, int experiance, String[] competences, String attentes) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.experiance = experiance;
+		this.competences = competences;
+		this.attentes = attentes;
+	}
+}
